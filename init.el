dc585c4022580022e41c805fe3262e23dfec40fe
zackteo <zackteo@gmail.com>
2020-02-24 10:56:33 +0800

    Added configurations in an org file

--- init.el
+++ init.el
@@ -1,25 +1,4 @@
-;; stuff automatically added
-;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (counsel swiper lorem-ipsum tabbar ace-window org-bullets which-key try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
-
-;; Our stuff
-
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -32,79 +11,21 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
-(use-package try
-	:ensure t)
-
-(use-package which-key
-	:ensure t 
-	:config
-	(which-key-mode))
 
 
-;; Org-mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-
-
-;; using swiper so ido no longer needed
-;;(setq ido-enable-flex-matching t)
-;;(setq ido-everywhere t)
-;;(ido-mode 1)
-
-(defalias 'list-buffers 'ibuffer)
-;; (defalias 'list-buffers 'ibuffer-other-window)
-
-; If you like a tabbar 
-;(use-package tabbar
-;  :ensure t
-;  :config
-;  (tabbar-mode 1))
-
-
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
-    ))
-
-
-;; it looks like counsel is a requirement for swiper
-(use-package counsel
-  :ensure t
-  )
-
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-display-style 'fancy))
-
-
-(use-package swiper
-  :ensure try
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
-
-(use-package avy
-  :ensure t
-  :bind ("M-s" . avy-goto-char))
+(org-babel-load-file (expand-file-name "~/.emacs.d/zackteo.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org Documents/newgtd.org")))
+ '(package-selected-packages
+   (quote
+    (plantuml-mode plantUML-mode org-noter exwm ox-latex org-pdfview htmlize ox-reveal zenburn-theme zeal-at-point which-key use-package try projectile org-bullets magit lsp-ui lsp-java jdee java-snippets eclim dap-mode counsel company-lsp color-theme-modern color-theme auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
